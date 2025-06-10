c0e6fe40d74c0c9138c71355982442c590697930
Laura <lauracrsantos157@gmail.com>
2025-06-10 14:51:03 -0300

    Fase 1 Término

---
+++ src/test/java/local/laura/OSApiApplication/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package local.laura.OSApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
